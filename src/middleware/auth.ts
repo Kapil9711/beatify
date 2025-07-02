742e6e13abad8332b7345c0dee5728941a57ff1f
kapil rajput <gemlay.dev4@gemlay.com>
2025-07-02 13:09:59 +0530

    added login form

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,58 +1,67 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
-import UserModel from '../model/user';
-import catchAsyncError from './catchAsyncError';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import UserModel from "../model/user";
+import catchAsyncError from "./catchAsyncError";
 
-import { NextFunction, Request, Response } from 'express';
-import CustomError from './customError';
-import pathVariable from '../config/pathVariables';
-import { ExtendedRequest } from '../types/express/request';
+import { NextFunction, Request, Response } from "express";
+import CustomError from "./customError";
+import pathVariable from "../config/pathVariables";
+import { ExtendedRequest } from "../types/express/request";
 
 // Check if the user is authenticated or not
-export const isAuthenticatedUser = catchAsyncError(async (req:ExtendedRequest, res:Response, next:NextFunction) => {
-  let token;
-  let path = req.path;
+export const isAuthenticatedUser = catchAsyncError(
+  async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    let token;
+    let path = req.originalUrl;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (
+      (req.originalUrl == "/api-docs" || req.originalUrl == "/api-docs/") &&
+      !token
+    ) {
+      return res.redirect(302, "/api-docs/login");
+    }
+
+    if (!token) {
+      return next(new CustomError("Login first to access this resource.", 401));
+    }
+
+    const decoded = jwt.verify(token, pathVariable.JWT_SECRET);
+    if (typeof decoded === "object" && decoded !== null) {
+      var payload = decoded as JwtPayload;
+    } else {
+      if (req.originalUrl == "/api-docs" || req.originalUrl == "/api-docs/") {
+        return res.redirect(302, "/api-docs/login");
+      }
+      throw new CustomError("Invalid jwt token", 401);
+    }
+
+    const user = await UserModel.findById(payload.id).select(
+      "isAdmin userName email profileImage"
+    );
+    if (!user) {
+      if (req.originalUrl == "/api-docs" || req.originalUrl == "/api-docs/") {
+        return res.redirect(302, "/api-docs/login");
+      }
+      throw new CustomError("User Not Found", 404);
+    }
+    req.user = user;
+    next();
   }
-
-  if(path == '/api-docs' && !token){
-    return res.redirect(302,'/api-docs/login')
-  }
-
-  if (!token) {
-    return next(new CustomError("Login first to access this resource.", 401));
-  }
-
-  
-  
-
-  const decoded  = jwt.verify(token, pathVariable.JWT_SECRET);
-  if(typeof decoded === 'object' && decoded !== null){
-    var payload = decoded as JwtPayload
-  }else {
-     if(path == '/api-docs'){
-    return res.redirect(302,'/api-docs/login')}
-    throw new CustomError('Invalid jwt token',401)
-  }
-  
-  const user = await UserModel.findById(payload.id).select('isAdmin userName email profileImage');
-  if(!user){
-    if(path == '/api-docs'){
-    return res.redirect(302,'/api-docs/login')}
-    throw new CustomError('User Not Found',404);
-  } 
-  req.user  = user;
-  next();
-});
+);
 
 // handling users roles
-export const authorize =  catchAsyncError(async (req:ExtendedRequest, res:Response, next:NextFunction) => {
-    if(req.user?.isAdmin) return next();
-     if(req.path == '/api-docs'){
-    return res.redirect(302,'/api-docs/login')}
-    throw new CustomError('Not Authorize To Access The Information',401)    
-})
+export const authorize = catchAsyncError(
+  async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    if (req.user?.isAdmin) return next();
+    if (req.originalUrl == "/api-docs" || req.originalUrl == "/api-docs/") {
+      return res.redirect(302, "/api-docs/login");
+    }
+    throw new CustomError("Not Authorize To Access The Information", 401);
+  }
+);
--- src/routes/api-docs/index.ts
+++ src/routes/api-docs/index.ts
@@ -1,15 +1,25 @@
-import {NextFunction, Response, Router} from 'express';
-import { authorize, isAuthenticatedUser } from '../../middleware/auth';
+import { NextFunction, Response, Router } from "express";
+import { authorize, isAuthenticatedUser } from "../../middleware/auth";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "../../swagger";
-import catchAsyncError from '../../middleware/catchAsyncError';
-import { ExtendedRequest } from '../../types/express/request';
+import catchAsyncError from "../../middleware/catchAsyncError";
+import { ExtendedRequest } from "../../types/express/request";
 const router = Router();
 
-router.route('/').get(isAuthenticatedUser ,authorize,swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-router.route('/login').get(catchAsyncError(async(req:ExtendedRequest,res:Response,next:NextFunction)=>{
-     res.render('login')
-}))
+router
+  .route("/")
+  .get(
+    isAuthenticatedUser,
+    authorize,
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec)
+  );
+router.route("/login").get(
+  catchAsyncError(
+    async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+      res.render("login");
+    }
+  )
+);
 
 export default router;
-
